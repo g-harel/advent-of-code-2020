7281f2576861f9159d3d3544485ce4e71b9eaf4a
g-harel <gabrielj.harel@gmail.com>
2021-04-07 23:35:29 -0500

    add better debug messages

--- days/19/solution.go
+++ days/19/solution.go
@@ -7,65 +7,75 @@ import (
 	"github.com/g-harel/advent-of-code-2020/lib"
 )
 
-// Consumes n items in str starting at index i.
-// Returns 0 when nothing is consumed.
 type Consumer struct {
-	about   string
-	consume func(i int, str string) (n int)
-	depth   string
+	// Consumes n items in str starting at index i.
+	// Returns 0 when nothing is consumed.
+	consume func(i int, str string, parents []string) (n int)
+	id      string
 }
 
-func (c *Consumer) Consume(i int, str string) int {
-	fmt.Println(c.depth + c.about)
-	c.depth += " "
-	n := c.consume(i, str)
-	c.depth = c.depth[1:]
+func (c *Consumer) Consume(i int, str string, parents []string) int {
+	p := strings.Join(parents, ",")
+	if c.id != "" {
+		parents = append(parents, c.id)
+	}
+	n := c.consume(i, str, parents)
+	if c.id != "" {
+		if n == 0 {
+			fmt.Printf("| %vFAIL | %v,%v\n", strings.Repeat(" ", len(str)-2), p, c.id)
+		} else {
+			fmt.Printf("| %v %v %v | %v,%v\n", str[:i], str[i:i+n], str[i+n:], p, c.id)
+		}
+	}
 	return n
 }
 
 func ByteConsumer(match byte) *Consumer {
-	return &Consumer{"ByteConsumer", func(i int, str string) int {
-		if str[i] == match {
-			return 1
-		}
-		return 0
-	}, ""}
+	return &Consumer{
+		func(i int, str string, parents []string) int {
+			if str[i] == match {
+				return 1
+			}
+			return 0
+		}, "b"}
 }
 
 func SuccessiveConsumer(consumers ...*Consumer) *Consumer {
-	return &Consumer{"SuccessiveConsumer", func(i int, str string) int {
-		nTotal := 0
-		for _, consumer := range consumers {
-			if i+nTotal >= len(str) {
-				return nTotal
+	return &Consumer{
+		func(i int, str string, parents []string) int {
+			nTotal := 0
+			for _, consumer := range consumers {
+				if i+nTotal >= len(str) {
+					return nTotal
+				}
+				n := consumer.Consume(nTotal+i, str, parents)
+				if n == 0 {
+					return 0
+				}
+				nTotal += n
 			}
-			n := consumer.Consume(nTotal+i, str)
-			if n == 0 {
-				return 0
-			}
-			nTotal += n
-		}
-		return nTotal
-	}, ""}
+			return nTotal
+		}, "s"}
 }
 
 func EitherConsumer(consumers ...*Consumer) *Consumer {
-	return &Consumer{"EitherConsumer", func(i int, str string) int {
-		for _, consumer := range consumers {
-			n := consumer.Consume(i, str)
-			if n != 0 {
-				return n
+	return &Consumer{
+		func(i int, str string, parents []string) int {
+			for _, consumer := range consumers {
+				n := consumer.Consume(i, str, parents)
+				if n != 0 {
+					return n
+				}
 			}
-		}
-		return 0
-	}, ""}
+			return 0
+		}, "e"}
 }
 
 func DeferredConsumer(fn func() *Consumer) *Consumer {
-	return &Consumer{"DeferredConsumer", func(i int, str string) int {
-		r := fn().Consume(i, str)
-		return r
-	}, ""}
+	return &Consumer{
+		func(i int, str string, parents []string) int {
+			return fn().Consume(i, str, parents)
+		}, ""}
 }
 
 func ParseConsumer(lines []string) *Consumer {
@@ -77,6 +87,7 @@ func ParseConsumer(lines []string) *Consumer {
 
 		if strings.HasPrefix(rule, "\"") {
 			rules[id] = ByteConsumer(rule[1])
+			rules[id].id = id
 			continue
 		}
 
@@ -96,9 +107,8 @@ func ParseConsumer(lines []string) *Consumer {
 			}
 			eitherConsumers = append(eitherConsumers, SuccessiveConsumer(successiveConsumers...))
 		}
-		k := EitherConsumer(eitherConsumers...)
-		k.about = id + " " + k.about
-		rules[id] = k
+		rules[id] = EitherConsumer(eitherConsumers...)
+		rules[id].id = id
 	}
 
 	return rules["0"]
@@ -109,7 +119,7 @@ func Part1() int {
 
 	matches := 0
 	for _, message := range groups[1] {
-		if ParseConsumer(groups[0]).Consume(0, message) == len(message) {
+		if ParseConsumer(groups[0]).Consume(0, message, []string{}) == len(message) {
 			matches++
 		}
 	}
@@ -132,7 +142,7 @@ func Part2() int {
 
 	matches := 0
 	for _, message := range groups[1] {
-		if ParseConsumer(groups[0]).Consume(0, message) == len(message) {
+		if ParseConsumer(groups[0]).Consume(0, message, []string{}) == len(message) {
 			println(message)
 			matches++
 		}
